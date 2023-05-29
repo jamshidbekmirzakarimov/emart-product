d7bbc08cb5bfec31bbb48b21e54d2cedb6acb6c7
Jamshidbek <jamshidbekmirzakarimov6@gmail.com>
2023-05-29 16:54:59 +0500

    hero section finished

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
-
+import BgImg from "../assets/images/bg-icon.png"
+import Products from './Products'
 const Home = () => {
   return (
     <div className='hero'>
-      <div className='card bg-dark text-white border-0'>
-        <img src="" alt="" />
+      <div className='card bg-dark border-0'>
+        <img src={BgImg} alt="" height={550} />
+        <div className='card-img-overlay d-flex flex-column justify-content-center'>
+          <div className='container'>
+            <h5 className='card-title display-3 fw-border mb-0'>NEW SEASON ARRIVALS</h5>
+            <p className='card-text lead fs-2'>CHECK OUT ALL THE TRENDS</p>
+          </div>
+        </div>
       </div>
+      <Products/>
     </div>
   )
 }
